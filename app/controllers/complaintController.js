891defdd9633ea7ed82316df1d9fb3625511e0f2
LAPTOP-RT0RDR2Q\HP <agilobitajm@gmail.com>
2023-09-30 08:46:20 +0700

    add get all complaints

--- app/controllers/complaintController.js
+++ app/controllers/complaintController.js
@@ -1,4 +1,67 @@
 const Complaint = require("../models/Complaint");
+const Feedback = require("../models/Feedback");
+
+async function getComplaints(req, res) {
+  try {
+    const complaints = await Feedback.aggregate([
+      {
+        $lookup: {
+          from: "complaints",
+          localField: "complaintID",
+          foreignField: "_id",
+          as: "complaint",
+        },
+      },
+      {
+        $unwind: "$complaint",
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "complaint.userID",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $unwind: "$user",
+      },
+      {
+        $project: {
+          _id: "$complaint._id",
+          title: "$complaint.title",
+          description: "$complaint.description",
+          keterangan: "$complaint.keterangan",
+          username: "$user.username",
+          upvote: 1,
+          downvote: 1,
+        },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          title: { $first: "$title" },
+          description: { $first: "$description" },
+          keterangan: { $first: "$keterangan" },
+          username: { $first: "$username" },
+          upvotes: { $sum: "$upvote" },
+          downvotes: { $sum: "$downvote" },
+        },
+      },
+      {
+        $sort: { upvotes: -1 },
+      },
+    ]);
+
+    // Send the complaints as a response
+    res.status(200).json({ complaints: complaints });
+  } catch (err) {
+    console.error("Error Complaint :", err);
+
+    // Handle any errors
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
 
 const addComplaint = (input) => {
   Complaint.insertMany(input);
@@ -9,12 +72,19 @@ const loadComplaints = () => {
 };
 
 const findComplaint = (id) => {
-  return Complaint.findOne({ _id: id});
+  return Complaint.findOne({ _id: id });
 };
 
 const deleteComplaint = (id) => {
   Complaint.deleteOne({ _id: id }).then((result) => {
-      return result;
+    return result;
   });
-}
-module.exports = { loadComplaints, addComplaint, findComplaint, deleteComplaint };
+};
+
+module.exports = {
+  getComplaints,
+  loadComplaints,
+  addComplaint,
+  findComplaint,
+  deleteComplaint,
+};
--- app/routes/complaintRoute.js
+++ app/routes/complaintRoute.js
@@ -1,47 +1,49 @@
 const express = require("express");
 const router = express.Router();
 
-const { loadComplaints, addComplaint, findComplaint, deleteComplaint } = require('../controllers/complaintController');
+const complaintController = require("../controllers/complaintController");
+
+router.get("/", complaintController.getComplaints);
 
 router.get("/complaints", async (_, res) => {
-    try {
-      const allComplaints = await loadComplaints();
-      res.status(200).json(allComplaints);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
-
-  router.post("/complaint", async (req, res) => {
-    try {
-      const result = await addComplaint(req.body);
-      console.log(result);
-      res.status(201).redirect("/api/complaints");
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
-
-  router.get("/complaint/detail/:id", async (req, res) => {
-    try {
-        const complaint = await findComplaint(req.params.id);
-        if (complaint === null) {
-            return res.status(404).json({ message: "Complaint NOT Found." });
-        }
-        res.status(201).json(complaint);
-    } catch (error) {
-        res.status(400).json({ message: "The Complaint NOT Found" });
-    }
+  try {
+    const allComplaints = await loadComplaints();
+    res.status(200).json(allComplaints);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
-  router.delete("/complaint/delete/:id", (req, res) => {
-    try {
-        const deletedComplaint = deleteComplaint(req.params.id);
-        console.log(deletedComplaint);
-        res.status(200).redirect("/api/complaints");
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+router.post("/complaint", async (req, res) => {
+  try {
+    const result = await addComplaint(req.body);
+    console.log(result);
+    res.status(201).redirect("/api/complaints");
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
-  
+
+router.get("/complaint/detail/:id", async (req, res) => {
+  try {
+    const complaint = await findComplaint(req.params.id);
+    if (complaint === null) {
+      return res.status(404).json({ message: "Complaint NOT Found." });
+    }
+    res.status(201).json(complaint);
+  } catch (error) {
+    res.status(400).json({ message: "The Complaint NOT Found" });
+  }
+});
+
+router.delete("/complaint/delete/:id", (req, res) => {
+  try {
+    const deletedComplaint = deleteComplaint(req.params.id);
+    console.log(deletedComplaint);
+    res.status(200).redirect("/api/complaints");
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 module.exports = router;

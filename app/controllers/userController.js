925425e7b73bdebbeea800119182a8b15221cdf6
LAPTOP-RT0RDR2Q\HP <agilobitajm@gmail.com>
2023-10-03 07:05:26 +0700

    update history and make object return profile instead array

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,11 +1,12 @@
 const User = require('../models/User');
 const Complaint = require("../models/Complaint");
+const Feedback = require('../models/Feedback');
 
 async function profileUser(req, res){
     const reqUser = req.user;
 
     try {
-        const user = await User.find({ _id: reqUser.userId }, 'username email');
+        const user = await User.findOne({ _id: reqUser.userId }, 'username email');
 
         res.status(200).json({ user: user });
     } catch (err) {
@@ -18,11 +19,11 @@ async function historyUser(req, res){
     const reqUser = req.user;
 
     try {
-        const complaints = await Complaint.find({ userID: reqUser.userId }).sort({ created_at: -1 });
+        const feedbacks = await Feedback.find({ userID: reqUser.userId }).sort({ created_at: -1 });
 
-        res.status(200).json({ complaints: complaints });
+        res.status(200).json({ feedbacks: feedbacks });
     } catch (err) {
-        console.error('Error get complaints:', err);
+        console.error('Error get feedbacks:', err);
         res.status(404).json({ message: 'Complaints not found' });
     }
 }

798772bbe31638395224d1b2aebf7318236db538
LAPTOP-RT0RDR2Q\HP <agilobitajm@gmail.com>
2023-10-03 08:38:39 +0700

    add vote feature, just in agi task

--- app/controllers/complaintController.js
+++ app/controllers/complaintController.js
@@ -63,6 +63,61 @@ async function getComplaints(req, res) {
   }
 }
 
+async function votes(req, res) {
+  const reqUser = req.user;
+  const { id } = req.body;
+  let { upvote, downvote } = req.body;
+
+  const isVoteNotValid =
+    (upvote === "0" && downvote === "0") ||
+    (upvote == "1" && downvote == "1") ||
+    (upvote === undefined && downvote === undefined);
+  if ((!id && !upvote && !downvote) || isVoteNotValid) {
+    return res.status(404).json({ message: "Bad request" });
+  }
+
+  const complaint = await Complaint.findOne({ _id: id }); // Find complain in database
+  if (!complaint) {
+    return res.status(404).json({ message: "Complaint not found" });
+  }
+
+  if (upvote === "upvote" || !downvote) {
+    upvote = 1;
+    downvote = 0;
+  }
+  if (downvote === "downvote" || !upvote) {
+    upvote = 0;
+    downvote = 1;
+  }
+
+  let feedback = await Feedback.findOne({
+    userID: reqUser.userId,
+    complaintID: id,
+  }); // Find feedback in
+  try {
+    if (feedback) {
+      if (feedback && feedback.upvote === upvote || feedback.downvote === downvote) {
+        return res.status(200).json({ message: "Please make changes." });
+      }
+      feedback.upvote = upvote;
+      feedback.downvote = downvote;
+    } else {
+      feedback = new Feedback({
+        userID: reqUser.userId,
+        complaintID: id,
+        upvote: upvote,
+        downvote: downvote,
+      });
+    }
+
+    await feedback.save();
+    return res.status(200).json({ feedback: feedback });
+  } catch (err) {
+    console.error("Error feedback:", err);
+  }
+  res.status(404).json({ message: "Feedback not found" });
+}
+
 const addComplaint = (input) => {
   Complaint.insertMany(input);
 };
@@ -83,6 +138,7 @@ const deleteComplaint = (id) => {
 
 module.exports = {
   getComplaints,
+  votes,
   loadComplaints,
   addComplaint,
   findComplaint,

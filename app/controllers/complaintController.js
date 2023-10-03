6c5f82470e8cd0d904ef28918b14fcf877675052
LAPTOP-RT0RDR2Q\HP <agilobitajm@gmail.com>
2023-10-04 06:59:15 +0700

    adding logic, user vote already or not

--- app/controllers/complaintController.js
+++ app/controllers/complaintController.js
@@ -1,55 +1,100 @@
 const Complaint = require("../models/Complaint");
 const Feedback = require("../models/Feedback");
 
+const jwt = require("jsonwebtoken");
+const accessToken = process.env.SECRET_TOKEN;
+
 async function getComplaints(req, res) {
   try {
-    const complaints = await Feedback.aggregate([
-      {
-        $lookup: {
-          from: "complaints",
-          localField: "complaintID",
-          foreignField: "_id",
-          as: "complaint",
-        },
-      },
-      {
-        $unwind: "$complaint",
-      },
+    const authHeader = req.header("Authorization");
+    const token = authHeader && authHeader.split(" ")[1];
+
+    let user = "";
+    if (token) {
+      const decodedToken = jwt.verify(token, accessToken);
+      user = decodedToken ?? "";
+    }
+
+    // gunakan aggregate pada model Complaint
+    const complaints = await Complaint.aggregate([
+      // tahap pertama: lookup user dan feedback
       {
         $lookup: {
           from: "users",
-          localField: "complaint.userID",
+          localField: "userID",
           foreignField: "_id",
           as: "user",
         },
       },
       {
+        $lookup: {
+          from: "feedbacks",
+          localField: "_id",
+          foreignField: "complaintID",
+          as: "feedbacks",
+        },
+      },
+      // tahap kedua: unwind user dan feedbacks
+      {
         $unwind: "$user",
       },
       {
-        $project: {
-          _id: "$complaint._id",
-          title: "$complaint.title",
-          description: "$complaint.description",
-          keterangan: "$complaint.keterangan",
-          username: "$user.username",
-          upvote: 1,
-          downvote: 1,
-        },
+        $unwind: "$feedbacks",
       },
+      // tahap ketiga: group berdasarkan _id complaint
       {
         $group: {
           _id: "$_id",
           title: { $first: "$title" },
           description: { $first: "$description" },
           keterangan: { $first: "$keterangan" },
-          username: { $first: "$username" },
-          upvotes: { $sum: "$upvote" },
-          downvotes: { $sum: "$downvote" },
+          // hitung total upvote dan downvote
+          totalUpvotes: { $sum: "$feedbacks.upvote" },
+          totalDownvotes: { $sum: "$feedbacks.downvote" },
+          // cek apakah user tertentu pernah memberikan feedback
+          feedbacks: {
+            $push: {
+              userID: "$feedbacks.userID",
+              upvote: "$feedbacks.upvote",
+              downvote: "$feedbacks.downvote",
+            },
+          },
+          vote: {
+            // tambahkan operator $first di sini
+            $first: {
+              $cond: [
+                // gunakan tanda kutip ganda di sini
+                {
+                  $setIsSubset: [
+                    [{ $toString: "$feedbacks.userID" }],
+                    [user.userId],
+                  ],
+                },
+                // gunakan tanda kutip ganda di sini juga
+                {
+                  $cond: [
+                    { $eq: ["$feedbacks.upvote", 1] },
+                    "upvote",
+                    "downvote",
+                  ],
+                },
+                null,
+              ],
+            },
+          },
         },
       },
+      // tahap keempat: project field-field yang ingin ditampilkan
       {
-        $sort: { upvotes: -1 },
+        $project: {
+          _id: 1,
+          title: 1,
+          description: 1,
+          keterangan: 1,
+          totalUpvotes: 1,
+          totalDownvotes: 1,
+          vote: 1,
+        },
       },
     ]);
 
@@ -96,7 +141,10 @@ async function votes(req, res) {
   }); // Find feedback in
   try {
     if (feedback) {
-      if (feedback && feedback.upvote === upvote || feedback.downvote === downvote) {
+      if (
+        (feedback && feedback.upvote === upvote) ||
+        feedback.downvote === downvote
+      ) {
         return res.status(200).json({ message: "Please make changes." });
       }
       feedback.upvote = upvote;
--- app/routes/complaintRoute.js
+++ app/routes/complaintRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const complaintController = require("../controllers/complaintController");
-const authentication = require('../middleware/authentication');
+const authentication = require("../middleware/authentication");
 
 router.get("/", complaintController.getComplaints);
-router.post("/votes", authentication, complaintController.votes);
+router.post("/votes", complaintController.votes);
 
 module.exports = router;

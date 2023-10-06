1e74f8dffa2dfe4e1f50184cdcdff5978505b5d3
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2023-10-07 02:54:26 +0700

    add comment to use middleware in routes

--- app/routes/voteRoute.js
+++ app/routes/voteRoute.js
@@ -8,6 +8,10 @@ const router = express.Router();
 if route should be authenticate
 add authentication function after url before controller
 example: router.get('/profile', **authentication**, userController.profileUser)
+
+if route should be authenticate but still request can be pass
+add passAuthentication function after url before controller
+example: router.get('/profile', **passAuthentication**, userController.profileUser)
 */
 
 router.put('/upvote/:id', authentication, voteController.upvoteComplaint)

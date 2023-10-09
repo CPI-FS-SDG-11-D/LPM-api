c6823f14374850d000404f71e5087bbc8271776a
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2023-10-10 00:02:47 +0700

    revised upvote and downvote

--- app/controllers/voteController.js
+++ app/controllers/voteController.js
@@ -5,54 +5,58 @@ async function upvoteComplaint(req, res){
     const reqUser = req.user;
     const reqComplaint = req.params;
     const complaint = await Complaint.findOne({ _id: reqComplaint.id }); // Find complain in database
-    // const feedback = await Feedback.findOne({ userID: reqUser.userId, complaintID: reqComplaint.id, }); // Find upvote in database
+    const feedback = await Feedback.findOne({ userID: reqUser.userId, complaintID: reqComplaint.id }); // Find upvote in database
 
-    // if (feedback) {
-    //     return res.status(409).json({ message: 'User already voted' });
-    // }
+    if(feedback) {
+        if (feedback.is_upvote == true || feedback.is_downvote == true) {
+            return res.status(409).json({ message: 'User already voted' });
+        }   
+    } else {
+        if (complaint) {
+            await Complaint.findByIdAndUpdate(reqComplaint.id, { totalUpvotes: complaint.totalUpvotes + 1 }, { new: true, runValidators: true }); // Update upvote complaint
+        } else {
+            return res.status(404).json({ message: 'Complaint not found' });
+        }    
 
-    // if (complaint) {
-    //     await Complaint.findByIdAndUpdate(reqComplaint.id, { upvote: complaint.upvote + 1 }, { new: true, runValidators: true }); // Update upvote complaint
-    // } else {
-    //     return res.status(404).json({ message: 'Complaint not found' });
-    // }
-
-    // try {
-    //     const newUpvote = new Upvote({ userID: reqUser.userId, complaintID: reqComplaint.id, count: 1 })
-    //     await newUpvote.save();
-
-    //     res.status(200).json({ upvote: complaint.upvote + 1 });
-    // } catch (err) {
-    //     console.error('Error save upvote:', err);
-    //     res.status(500).json({ message: 'Internal server error' });
-    // }
+        try {
+            const newFeedback = new Feedback({ userID: reqUser.userId, complaintID: reqComplaint.id, is_upvote: true }) // Save feedback upvotes
+            await newFeedback.save();
+    
+            res.status(200).json({ totalUpvotes: complaint.totalUpvotes + 1 });
+        } catch (err) {
+            console.error('Error save upvote:', err);
+            res.status(500).json({ message: 'Internal server error' });
+        }    
+    }
 }
 
 async function downvoteComplaint(req, res){
     const reqUser = req.user;
     const reqComplaint = req.params;
     const complaint = await Complaint.findOne({ _id: reqComplaint.id }); // Find complain in database
-    // const downvote = await Downvote.findOne({ userID: reqUser.userId, complaintID: reqComplaint.id, }); // Find downvote in database
+    const feedback = await Feedback.findOne({ userID: reqUser.userId, complaintID: reqComplaint.id, }); // Find downvote in database
 
-    // if (downvote) {
-    //     return res.status(409).json({ message: 'User already voted' });
-    // }
+    if(feedback) {
+        if (feedback.is_upvote == true || feedback.is_downvote == true) {
+            return res.status(409).json({ message: 'User already voted' });
+        }   
+    } else {
+        if (complaint) {
+            await Complaint.findByIdAndUpdate(reqComplaint.id, { totalDownvotes: complaint.totalDownvotes + 1 }, { new: true, runValidators: true }); // Update upvote complaint
+        } else {
+            return res.status(404).json({ message: 'Complaint not found' });
+        } 
 
-    // if (complaint) {
-    //     await Complaint.findByIdAndUpdate(reqComplaint.id, { downvote: complaint.downvote + 1 }, { new: true, runValidators: true }); // Update downvote complaint
-    // } else {
-    //     return res.status(404).json({ message: 'Complaint not found' });
-    // }
-
-    // try {
-    //     const newDownvote = new Downvote({ userID: reqUser.userId, complaintID: reqComplaint.id, count: 1 })
-    //     await newDownvote.save();
-
-    //     res.status(200).json({ upvote: complaint.upvote + 1 });
-    // } catch (err) {
-    //     console.error('Error save downvote:', err);
-    //     res.status(500).json({ message: 'Internal server error' });
-    // }
+        try {
+            const newFeedback = new Feedback({ userID: reqUser.userId, complaintID: reqComplaint.id, is_downvote: true }) // Save feedback downvotes
+            await newFeedback.save();
+    
+            res.status(200).json({ totalDownvotes: complaint.totalDownvotes + 1 });
+        } catch (err) {
+            console.error('Error save upvote:', err);
+            res.status(500).json({ message: 'Internal server error' });
+        }    
+    }
 }
 
 module.exports = { upvoteComplaint, downvoteComplaint }

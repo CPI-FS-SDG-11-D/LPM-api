76390860e2827d77da2fbd509bea91131da4ecd0
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2023-10-11 22:17:26 +0700

    fix middleware passAuthentication access

--- app/middleware/passAuthentication.js
+++ app/middleware/passAuthentication.js
@@ -6,11 +6,7 @@ const accessToken = process.env.SECRET_TOKEN; // Set Secret Token
 async function passAuthentication(req, res, next) {
     const authHeader = req.header('Authorization');
     const token = authHeader && authHeader.split(' ')[1];
-  
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-  
+    
     try {
         const decodedToken = jwt.verify(token, accessToken);    
         req.user = decodedToken;

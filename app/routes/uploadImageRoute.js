2b43407bc557a807d040005c1f21478e5f1b41e5
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2023-10-17 12:41:58 +0700

    fix error feature upload image

--- app/routes/uploadImageRoute.js
+++ app/routes/uploadImageRoute.js
@@ -4,8 +4,8 @@ const uploadImageController = require('../controllers/uploadImageController');
 const authentication = require('../middleware/authentication');
 
 const router = express.Router();
-const uploadUser = multer({ dest: '/src/images/profile' });
-const uploadComplaint = multer({ dest: '/src/images/complaint' });
+const uploadUser = multer({ dest: 'src/images/user' });
+const uploadComplaint = multer({ dest: 'src/images/complaint/' });
 
 /*
 if route should be authenticate
@@ -14,10 +14,11 @@ example: router.get('/profile', **authentication**, userController.profileUser)
 
 if route should be authenticate but still request can be pass
 add passAuthentication function after url before controller
-example: router.get('/profile', **passAuthentication**, userController.profileUser)
+example: router.post('/upload-user', passAuthentication, uploadUser.single('image'), uploadImageController.imageUser)
+
 */
 
-router.post('/upload-user', uploadUser.single('image'), authentication, uploadImageController.imageUser)
-router.post('/upload-complaint', uploadComplaint.single('image'), authentication, uploadImageController.imageComplaint)
+router.post('/upload-user', authentication, uploadUser.single('image'), uploadImageController.imageUser)
+router.post('/upload-complaint', authentication, uploadComplaint.single('image'), uploadImageController.imageComplaint)
   
 module.exports = router;

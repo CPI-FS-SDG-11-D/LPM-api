8468c8989f0542a05d1f4c52dfc9865ee5babf7f
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2023-11-06 22:26:00 +0700

    revised vote logic

--- app/controllers/voteController.js
+++ app/controllers/voteController.js
@@ -8,8 +8,28 @@ async function upvoteComplaint(req, res){
     const feedback = await Feedback.findOne({ userID: reqUser.userId, complaintID: reqComplaint.id }); // Find vote in database
 
     if(feedback) {
-        if (feedback.is_upvote == true || feedback.is_downvote == true) {
-            return res.status(409).json({ message: 'User already voted' });
+        if (feedback.is_upvote == true) {
+            if (complaint) {
+                await Complaint.findByIdAndUpdate(reqComplaint.id, { totalUpvotes: complaint.totalUpvotes - 1 }, { new: true, runValidators: true }); // Update upvote complaint
+            } else {
+                return res.status(404).json({ message: 'Complaint not found'});
+            }    
+    
+            try {
+                await Feedback.deleteOne({ userID: reqUser.userId, complaintID: reqComplaint.id, is_upvote: true });
+    
+                const responseFeedback = {
+                    is_upvote: false,
+                    is_downvote: false
+                };
+        
+                res.status(200).json({ feedback: responseFeedback, totalUpvotes: complaint.totalUpvotes - 1 });
+            } catch (err) {
+                console.error('Error save upvote:', err);
+                res.status(500).json({ message: 'Internal server error' });
+            }    
+        } else if (feedback.is_downvote == true) {
+            res.status(409).json({ message: 'User already voted' });
         }   
     } else {
         if (complaint) {
@@ -42,8 +62,28 @@ async function downvoteComplaint(req, res){
     const feedback = await Feedback.findOne({ userID: reqUser.userId, complaintID: reqComplaint.id, }); // Find vote in database
 
     if(feedback) {
-        if (feedback.is_upvote == true || feedback.is_downvote == true) {
-            return res.status(409).json({ message: 'User already voted' });
+        if (feedback.is_downvote == true) {
+            if (complaint) {
+                await Complaint.findByIdAndUpdate(reqComplaint.id, { totalDownvotes: complaint.totalDownvotes - 1 }, { new: true, runValidators: true }); // Update upvote complaint
+            } else {
+                return res.status(404).json({ message: 'Complaint not found'});
+            }    
+    
+            try {
+                await Feedback.deleteOne({ userID: reqUser.userId, complaintID: reqComplaint.id, is_downvote: true });
+    
+                const responseFeedback = {
+                    is_upvote: false,
+                    is_downvote: false
+                };
+        
+                res.status(200).json({ feedback: responseFeedback, totalDownvotes: complaint.totalDownvotes - 1 });
+            } catch (err) {
+                console.error('Error save upvote:', err);
+                res.status(500).json({ message: 'Internal server error' });
+            } 
+        } else if (feedback.is_upvote == true) {
+            res.status(409).json({ message: 'User already voted' });
         }   
     } else {
         if (complaint) {

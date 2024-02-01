a6f069224bd9b872ace61becd51f1bd4970db000
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-02-01 11:29:32 +0700

    make function vote more efficient

--- app/controllers/voteController.js
+++ app/controllers/voteController.js
@@ -15,23 +15,22 @@ async function upvoteComplaint(req, res){
     
         if (feedback) {
             if (feedback.is_upvote) {
-                await Complaint.findByIdAndUpdate(reqComplaint.id, { totalUpvotes: complaint.totalUpvotes - 1 }, { new: true, runValidators: true });
-                await Feedback.deleteOne({ userID: reqUser.userId, complaintID: reqComplaint.id, is_upvote: true });
+                await Promise.all([
+                    Complaint.findByIdAndUpdate(reqComplaint.id, { totalUpvotes: -1 }, { new: true, runValidators: true }),
+                    Feedback.deleteOne({ userID: reqUser.userId, complaintID: reqComplaint.id, is_upvote: true })
+                ]);
     
-                const responseFeedback = { is_upvote: false, is_downvote: false };
-
-                res.status(200).json({ feedback: responseFeedback, totalUpvotes: complaint.totalUpvotes - 1 });
+                res.status(200).json({ feedback: { is_upvote: false, is_downvote: false }, totalUpvotes: complaint.totalUpvotes - 1 });
             } else if (feedback.is_downvote) {
                 res.status(409).json({ message: 'User already voted' });
             }
         } else {
-            await Complaint.findByIdAndUpdate(reqComplaint.id, { totalUpvotes: complaint.totalUpvotes + 1 }, { new: true, runValidators: true });
+            await Promise.all([
+                Complaint.findByIdAndUpdate(reqComplaint.id, { totalUpvotes: + 1 }, { new: true, runValidators: true }),
+                Feedback.create({ userID: reqUser.userId, complaintID: reqComplaint.id, is_upvote: true })
+            ]);
     
-            const newFeedback = new Feedback({ userID: reqUser.userId, complaintID: reqComplaint.id, is_upvote: true });
-            await newFeedback.save();
-    
-            const responseFeedback = { is_upvote: newFeedback.is_upvote, is_downvote: newFeedback.is_downvote };
-            res.status(200).json({ feedback: responseFeedback, totalUpvotes: complaint.totalUpvotes + 1 });
+            res.status(200).json({ feedback: { is_upvote: true, is_downvote: false }, totalUpvotes: complaint.totalUpvotes + 1 });
         }
     } catch (err) {
         console.error('Error save upvote:', err);
@@ -52,24 +51,23 @@ async function downvoteComplaint(req, res){
         }    
     
         if (feedback) {
-            if (feedback.is_downvote) {
-                await Complaint.findByIdAndUpdate(reqComplaint.id, { totalDownvotes: complaint.totalDownvotes - 1 }, { new: true, runValidators: true });
-                await Feedback.deleteOne({ userID: reqUser.userId, complaintID: reqComplaint.id, is_downvote: true });
-    
-                const responseFeedback = { is_upvote: false, is_downvote: false };
+            if (feedback.is_downvote) {    
+                await Promise.all([
+                    Complaint.findByIdAndUpdate(reqComplaint.id, { totalDownvotes: -1 }, { new: true, runValidators: true }),
+                    Feedback.deleteOne({ userID: reqUser.userId, complaintID: reqComplaint.id, is_downvote: true })
+                ]);
                 
-                res.status(200).json({ feedback: responseFeedback, totalDownvotes: complaint.totalDownvotes - 1 });
+                res.status(200).json({ feedback: { is_upvote: false, is_downvote: false }, totalDownvotes: complaint.totalDownvotes - 1 });
             } else if (feedback.is_downvote) {
                 res.status(409).json({ message: 'User already voted' });
             }
         } else {
-            await Complaint.findByIdAndUpdate(reqComplaint.id, { totalDownvotes: complaint.totalDownvotes + 1 }, { new: true, runValidators: true });
+            await Promise.all([
+                Complaint.findByIdAndUpdate(reqComplaint.id, { totalUpvtotalDownvotesotes: + 1 }, { new: true, runValidators: true }),
+                Feedback.create({ userID: reqUser.userId, complaintID: reqComplaint.id, is_downvote: true })
+            ]);
     
-            const newFeedback = new Feedback({ userID: reqUser.userId, complaintID: reqComplaint.id, is_downvote: true });
-            await newFeedback.save();
-    
-            const responseFeedback = { is_upvote: newFeedback.is_upvote, is_downvote: newFeedback.is_downvote };
-            res.status(200).json({ feedback: responseFeedback, totalDownvotes: complaint.totalDownvotes + 1 });
+            res.status(200).json({ feedback: { is_upvote: false, is_downvote: true }, totalDownvotes: complaint.totalDownvotes + 1 });
         }
     } catch (err) {
         console.error('Error save upvote:', err);
